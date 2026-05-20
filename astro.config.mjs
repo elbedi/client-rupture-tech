c215e7805ac52fbc1c9f2ed28a3f771bd4a895c0
BenitoLM <benito.lopez.tecno@gmail.com>
2026-05-20 13:23:52 -0600

    conflict seolt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://rupture.tech",
